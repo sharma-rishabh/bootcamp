3831e7970726c37505fa2b883c324a3b5ccb5bb6
Risabh Sharma <99526730+sharma-rishabh@users.noreply.github.com>
2022-09-16 13:00:09 +0530

    Added size to parking lot

--- src/com/tw/step/assignment4/parking/ParkingLot.java
+++ src/com/tw/step/assignment4/parking/ParkingLot.java
@@ -4,12 +4,22 @@ import java.util.ArrayList;
 
 public class ParkingLot {
   private final ArrayList<Car> cars;
+  private final int size;
 
-  public ParkingLot() {
+  public ParkingLot(int size) {
+    this.size = size;
     this.cars = new ArrayList<Car>();
   }
 
   public boolean add(Car car) {
     return this.cars.add(car);
+  }
+
+  public boolean contains(Car car) {
+    return this.cars.contains(car);
+  }
+
+  public boolean isFull() {
+    return this.cars.size() == this.size;
   }
 }
--- test/com/tw/step/assignment4/parking/ParkingLotTest.java
+++ test/com/tw/step/assignment4/parking/ParkingLotTest.java
@@ -8,7 +8,32 @@ class ParkingLotTest {
 
   @Test
   void shouldAddACarToParkingLot() {
-    ParkingLot parkingLot = new ParkingLot();
+    ParkingLot parkingLot = new ParkingLot(1);
     assertTrue(parkingLot.add(new Car()));
+  }
+
+  @Test
+  void shouldTellIfCarIsInParkingLot() {
+    ParkingLot parkingLot = new ParkingLot(2);
+    Car firstCar = new Car();
+    Car secondCar = new Car();
+
+    parkingLot.add(firstCar);
+
+    assertTrue(parkingLot.contains(firstCar));
+    assertFalse(parkingLot.contains(secondCar));
+  }
+
+  @Test
+  void shouldTellIfParkingLotIsFull() {
+    ParkingLot parkingLot = new ParkingLot(2);
+    Car firstCar = new Car();
+    Car secondCar = new Car();
+
+    parkingLot.add(firstCar);
+    assertFalse(parkingLot.isFull());
+
+    parkingLot.add(secondCar);
+    assertTrue(parkingLot.isFull());
   }
 }
